7196c74353e88119f137d407cc0199abe47628ef
Lijin-mu <lijin@2hatslogic.com>
2025-03-12 01:10:20 +0530

    builds

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -12,6 +12,9 @@ function ProductList({ onHomeClick }) {
     const [addedToCart, setAddedToCart] = useState(false);
     const dispatch = useDispatch();
 
+     const cartCount = useSelector(state => state.cart.items.length);
+     console.log(cartCount);
+
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -291,7 +294,7 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><span className='cart_quantity_count'>{cartCount}</span> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
                 </div>
             </div>
             {!showCart ? (
@@ -299,7 +302,7 @@ function ProductList({ onHomeClick }) {
 
                 {plants.map((category, index) => (
                     <div key={index}>
-                        <h1><div>{category.category}</div></h1>
+                        <h1><div className='title'>{category.category}</div></h1>
                         <div className="product-list">
                             {category.plants.map((plant, plantIndex) => (
                             <div className="product-card" key={plantIndex}>
